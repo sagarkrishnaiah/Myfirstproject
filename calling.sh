23d8ab67d76daf5a76d3263f0462f0ac38d09bcd
sagar <azdosagar@outlook.com>
2024-06-14 16:52:15 +0530

    Shell script practice

---
+++ calling.sh
@@ -0,0 +1,3 @@
+./printing.sh $1 $1 $3 $4
+
+echo "am in calling.sh"
---
+++ printing.sh
@@ -0,0 +1,5 @@
+echo "welcome $1 & $2 to this session of devops "
+
+echo "Your trainer will be $3 for this session of devops "
+
+echo "Batch time is $4"
